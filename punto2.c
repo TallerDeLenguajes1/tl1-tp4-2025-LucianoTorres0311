7ea3ff7a9164c157e1267e52ad758d3b1586b741
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-27 20:11:15 -0300

    mejora de lectura del codigo

--- punto2.c
+++ punto2.c
@@ -10,24 +10,43 @@ int Duracion; // entre 10 – 100
 }Tarea;
  typedef struct {
 Tarea T;
-Nodo *Siguiente;
+struct Nodo *Siguiente;
 }Nodo;
 
-Nodo * crearListaVacia();
-Nodo *CrearNodo(Tarea T);
-void insertarNodos(Nodo **TareasPendientes, Nodo *NuevoNodo);
 
+Nodo * crearListaVacia();
+Nodo * CrearNodo(Tarea T);
+void insertarNodos(Nodo ** Start, Nodo *Nodo);
+Tarea CrearTarea(int id);
+void MostrarLista(Nodo *Start);
 
 int main()
 {
+    srand(time(NULL));
     int opcion;
-    Nodo * TareasPendientes = crearListaVacia();
-    Nodo * TareasRealizadas = crearListaVacia();
-    printf("Ingrese la opcion que desee utilizar \n");
+    int id = 1000;
+    Nodo *TareasPendientes = crearListaVacia();
+    Nodo *TareasRealizadas = crearListaVacia();
+    
     do 
     {
-        
-    } while (opcion == 0);
+        printf("Ingrese la opcion que desee utilizar \n");
+        scanf("%d", &opcion);
+        Nodo * nuevoNodo;
+        fflush(stdin);
+        switch (opcion)
+        {
+        case 1:
+            ;
+            insertarNodos(&TareasPendientes, CrearNodo(CrearTarea(id++)));
+            break;
+        case 2:
+            MostrarLista(TareasPendientes);
+        break;
+        default:
+            break;
+        }
+    } while (opcion != 0);
     
     return 0;
 }
@@ -42,10 +61,50 @@ Nodo *Nodos = (Nodo *)malloc(sizeof(Nodo));
 
 Nodo *crearListaVacia()
 {
-    Return NULL;
+    return NULL;
 }
-void insertarNodos(Nodo **Start, Nodo *NuevoNodo)
+ void insertarNodos(Nodo ** Start, Nodo *Nodo)
 {
-    NuevoNodo->Siguiente = *Start;
-    *Start = NuevoNodo;
+    Nodo->Siguiente = *Start;
+    *Start = Nodo;
+}
+ 
+Tarea  CrearTarea(int id)
+{
+    Tarea Tareas;
+    char * buff = (char*)malloc(1000 * sizeof(char));
+    int aux;
+    int dura;
+    printf("Ingrese la descripcion de la tarea \n");
+    gets(buff);
+    do
+    {
+        printf("Ingrese la duracion de la tarea entre 10 y 100\n");
+        scanf("%d", &dura);
+        fflush(stdin);
+    } while (dura > 100 || dura < 10);
+    aux = strlen(buff);
+    Tareas.Descripcion = (char *)malloc((aux + 1)* sizeof(char));
+    strcpy(Tareas.Descripcion, buff);
+    Tareas.Duracion = dura;
+    Tareas.TareaID = id;
+    free(buff);
+    return Tareas;
+}
+
+
+
+void MostrarLista(Nodo *Start) {
+    if (Start == NULL)
+    {
+        printf("\nLa lista se encuentra vacia\n");
+    }
+    while (Start != NULL)
+    {
+        printf("\n---TAREA---");
+        printf("\nID: %d ", Start->T.TareaID);
+        printf("\nDescripcion: %s", Start->T.Descripcion);
+        printf("\nDuracion de la tarea: %d\n", Start->T.Duracion);
+        Start = Start->Siguiente;
+    }
 }
